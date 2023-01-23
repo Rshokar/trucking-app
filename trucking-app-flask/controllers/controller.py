47b16594d45fd1947e97802661462fa0a8f92b33
rodribege <rodribege@gmail.com>
2023-01-23 12:06:14 -0800

    Add to controller outline

--- trucking-app-flask/controllers/controller.py
+++ trucking-app-flask/controllers/controller.py
@@ -1,2 +1,6 @@
 # Controller is generic parent class of all controlers
 # supports POST, GET, UPDATE, DELETE HTTP methods
+# Controller takes care of business logic
+# Routes route to controller
+# Controller validates input and make sure it works
+# Validation goes in models
