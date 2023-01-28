035095ea164eef75f6f8a4bb796041c0256cd2fe
Ryan Odribege <rodribege@my.bcit.ca>
2023-01-28 11:55:29 -0800

    Remove (dev-only) template from final version

--- trucking-app-flask/models/company.py
+++ trucking-app-flask/models/company.py
@@ -1,24 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, CHAR
 from models.model import Base
 
-
-class User(Base):
-    __tablename__ = 'users'
-    id = Column("id", Integer, primary_key=True)
-    type = Column("type", String(5))
-    password = Column("password", String(100))
-    email = Column("email", String(100))
-
-    def __init__(self, id, type, email, password) -> None:
-        self.id = id
-        self.type = type
-        self.email = email
-        self.password = password
-
-    def __repr__(self):
-        return f"({self.id}) {self.type} {self.email} {self.password}"
-
-
 class Company(Base):
     __tablename__ = 'companies'
     company_id = Column('company_id', Integer, primary_key=True)
