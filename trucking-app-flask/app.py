0f5d4903e9ec955b18f357a477119ba5836b95df
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-01-28 15:18:37 -0800

    I think i got it working

--- trucking-app-flask/app.py
+++ trucking-app-flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+
 # DB
 from config.db import engine
 from models.model import Base
@@ -14,31 +15,32 @@ from routes.user import user
 
 # Load env variables
 from dotenv import load_dotenv
-load_dotenv()
 
-app = Flask(__name__)
+def create_app(): 
+    load_dotenv()
 
-# Register all endpoints
-app.register_blueprint(user, url_prefix="/user")
-app.register_blueprint(auth, url_prefix="/auth")
-app.register_blueprint(billing_ticket, url_prefix="/billing_ticket")
-app.register_blueprint(rfo, url_prefix="/rfo")
-app.register_blueprint(dispatch, url_prefix="/dispatch")
-app.register_blueprint(company, url_prefix="/company")
+    app = Flask(__name__)
 
-# Create DB Tables
-try:
-    print("--|--CREATING TABLES--|--")
-    Base.metadata.create_all(engine)
-except Exception as e:
-    print("Error:", e)
+    # Register all endpoints
+    app.register_blueprint(user, url_prefix="/user")
+    app.register_blueprint(auth, url_prefix="/auth")
+    app.register_blueprint(billing_ticket, url_prefix="/billing_ticket")
+    app.register_blueprint(rfo, url_prefix="/rfo")
+    app.register_blueprint(dispatch, url_prefix="/dispatch")
+    app.register_blueprint(company, url_prefix="/company")
 
-try:
-    # Connect to the database
-    connection = engine.connect()
-    print("--|--Connection to the database is successful--|--")
-except Exception as e:
-    print("Error:", e)
+    # Create DB Tables
+    try:
+        print("--|--CREATING TABLES--|--")
+        Base.metadata.create_all(engine)
+    except Exception as e:
+        print("Error:", e)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    try:
+        # Connect to the database
+        connection = engine.connect()
+        print("--|--Connection to the database is successful--|--")
+    except Exception as e:
+        print("Error:", e)
+
+    return app
