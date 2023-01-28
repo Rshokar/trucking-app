ee5e9572fcb055e8b046578e654ef1979c913c8c
Ryan Odribege <rodribege@my.bcit.ca>
2023-01-28 13:49:24 -0800

    Complete billing tickets model (with comment on future design)

--- trucking-app-flask/models/billing_tickets.py
+++ trucking-app-flask/models/billing_tickets.py
@@ -1,6 +1,19 @@
-class Billing_Tickets:
+from sqlalchemy import Column, Integer, String, ForeignKey, CHAR
+from models.model import Base
+
+class BillingTickets(Base):
+    __tablename__ = 'billing_tickets'
+    bill_id = Column("bill_id", Integer, primary_key=True)
+    rof_id = Column("rof_id", Integer, ForeignKey(rof.rof_id))
+    ticket_number = Column("ticket_number", Integer)
+    # We won't ever need an image ID, right? If not, how will the image ID be integrated with the rest of the app
+    image_id = Column("image_id", Integer)
+
     def __init__(self, id, rof, ticket_number, image_id):
-        self.id = id
-        self.rof = rof
+        self.bill_id = id
+        self.rof_id = rof
         self.ticket_number = ticket_number
         self.image_id = image_id
+    
+    def __repr__(self):
+        return f"({self.bill_id}) {self.rof_id} {self.ticket_number} {self.image_id}"
