25e5f27ce638fb042d3670bb45901913f9be518d
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-02-27 18:19:45 -0800

    Added test test_update_a_operator_with_duplicate_email

--- trucking-app-flask/tests/test_operator.py
+++ trucking-app-flask/tests/test_operator.py
@@ -4,9 +4,10 @@ from config_test import app, client, session, user, company, customer, dispatch,
 
 END_POINT = "v1/company/operators"
 
+
 def test_get_operator(client, operator):
     """_summary_
-        
+
         Get a operator
     Args:
         clinet (_type_): _description_
@@ -22,9 +23,10 @@ def test_get_operator(client, operator):
     assert "operator_name" in data.keys()
     assert "operator_email" in data.keys()
 
+
 def test_get_non_existant_user(client):
     """_summary_
-        
+
         Get a operator
     Args:
         clinet (_type_): _description_
@@ -36,6 +38,7 @@ def test_get_non_existant_user(client):
     assert res.status_code == 404
     assert "error" in data.keys()
 
+
 def test_create_a_operator(client, company):
     """_summary_
         Creates a valid Operator
@@ -43,22 +46,23 @@ def test_create_a_operator(client, company):
         client (_type_): _description_
         company {Company}: A Company Object from DB
     """
-    
+
     payload = {
-        "company_id":company.company_id, 
-        "operator_name": "Keving Gates", 
+        "company_id": company.company_id,
+        "operator_name": "Keving Gates",
         "operator_email": "gator@gatertown.us"
     }
-    
+
     res = client.post(f"/{END_POINT}/", json=payload)
     data = res.json
-    
+
     assert res.status_code == 201
     assert "operator_id" in data.keys()
     assert "company_id" in data.keys()
     assert "operator_name" in data.keys()
     assert "operator_email" in data.keys()
-    
+
+
 def test_create_operator_missing_attribute(client, company):
     """_summary_
         Try's to create a operator with missing attributes
@@ -66,32 +70,33 @@ def test_create_operator_missing_attribute(client, company):
         client (_type_): _description_
         company {Company}: A Company Object from DB
     """
-    
+
     payload = {
-        "operator_name": "Keving Gates", 
+        "operator_name": "Keving Gates",
         "operator_email": "gator@gatertown.us"
     }
-    
+
     res = client.post(f"/{END_POINT}/", json=payload)
     assert res.status_code == 400
-    
+
     payload = {
         "company_id": company.company_id,
         "operator_email": "gator@gatertown.us"
     }
-    
+
     res = client.post(f"/{END_POINT}/", json=payload)
     assert res.status_code == 400
-    
+
     payload = {
         "company_id": company.company_id,
-        "operator_name": "Keving Gates", 
+        "operator_name": "Keving Gates",
     }
-    
+
     res = client.post(f"/{END_POINT}/", json=payload)
     assert res.status_code == 400
-        
-def test_create_operator_invalid_attributes(client, company): 
+
+
+def test_create_operator_invalid_attributes(client, company):
     """_summary_
         Trying to create operators with invalid attributes
     Args:
@@ -100,82 +105,80 @@ def test_create_operator_invalid_attributes(client, company):
     """
     payload = {
         "company_id": "ABC123",
-        "operator_name": "Keving Gates", 
+        "operator_name": "Keving Gates",
         "operator_email": "gator@gatertown.us"
     }
-    
-    res = client.post(f"/{END_POINT}/", json=payload)
-    assert res.status_code == 400
-    
-    
-    payload = {
-        "company_id": company.company_id,
-        "operator_name": "", 
-        "operator_email": "gator@gatertown.us"
-    }
-    
-    res = client.post(f"/{END_POINT}/", json=payload)
-    assert res.status_code == 400
-    
-    
-    payload = {
-        "company_id": company.company_id,
-        "operator_name": "Keving Gates", 
-        "operator_email": "gatorgatertown.us"
-    }
-    
-    res = client.post(f"/{END_POINT}/", json=payload)
-    assert res.status_code == 400
-    
-    payload = {
-        "company_id": company.company_id,
-        "operator_name": "Keving Gates", 
-        "operator_email": "gator@gatertownus"
-    }
-    
-    res = client.post(f"/{END_POINT}/", json=payload)
-    assert res.status_code == 400
-    
-    payload = {
-        "company_id": company.company_id,
-        "operator_name": "Keving Gates", 
-        "operator_email": "gator@gatertown."
-    }
-    
-    res = client.post(f"/{END_POINT}/", json=payload)
-    assert res.status_code == 400
-    
-    
-    payload = {
-        "company_id": company.company_id,
-        "operator_name": "Keving Gates", 
-        "operator_email": "@gatertown.us"
-    }
-    
+
     res = client.post(f"/{END_POINT}/", json=payload)
     assert res.status_code == 400
 
-def test_create_operator_with_email_already_taken(client, operator): 
+    payload = {
+        "company_id": company.company_id,
+        "operator_name": "",
+        "operator_email": "gator@gatertown.us"
+    }
+
+    res = client.post(f"/{END_POINT}/", json=payload)
+    assert res.status_code == 400
+
+    payload = {
+        "company_id": company.company_id,
+        "operator_name": "Keving Gates",
+        "operator_email": "gatorgatertown.us"
+    }
+
+    res = client.post(f"/{END_POINT}/", json=payload)
+    assert res.status_code == 400
+
+    payload = {
+        "company_id": company.company_id,
+        "operator_name": "Keving Gates",
+        "operator_email": "gator@gatertownus"
+    }
+
+    res = client.post(f"/{END_POINT}/", json=payload)
+    assert res.status_code == 400
+
+    payload = {
+        "company_id": company.company_id,
+        "operator_name": "Keving Gates",
+        "operator_email": "gator@gatertown."
+    }
+
+    res = client.post(f"/{END_POINT}/", json=payload)
+    assert res.status_code == 400
+
+    payload = {
+        "company_id": company.company_id,
+        "operator_name": "Keving Gates",
+        "operator_email": "@gatertown.us"
+    }
+
+    res = client.post(f"/{END_POINT}/", json=payload)
+    assert res.status_code == 400
+
+
+def test_create_operator_with_email_already_taken(client, operator):
     """_summary_
         Trie to create an operator with an email already take
     Args:
         client (_type_): _description_
         operator (_type_): _description_
     """
-    
-    
+
     payload = {
-        "company_id":operator.company_id, 
-        "operator_name": "Keving Gates", 
+        "company_id": operator.company_id,
+        "operator_name": "Keving Gates",
         "operator_email": operator.operator_email
     }
-    
+
     res = client.post(f"/{END_POINT}/", json=payload)
     data = res.json
 
     assert res.status_code == 400
     assert "error" in data.keys()
-    
+
+
 def test_update_a_operator(client, operator):
     """_summary_
         Updates an operator
@@ -183,18 +186,18 @@ def test_update_a_operator(client, operator):
         client (_type_): _description_
         company {Company}: A Company Object from DB
     """
-    
+
     payload = {
-        "company_id": operator.company_id, 
-        "operator_name": "Kevin Stain", 
+        "company_id": operator.company_id,
+        "operator_name": "Kevin Stain",
         "operator_email": "alegator@gatorbay.org"
     }
-    
+
     res = client.put(f"/{END_POINT}/{operator.operator_id}", json=payload)
     data = res.json
-    
+
     print(f"DATA: {data}")
-    
+
     print(data)
     assert res.status_code == 200
     assert "operator_id" in data.keys()
@@ -203,6 +206,7 @@ def test_update_a_operator(client, operator):
     assert data["operator_name"] == payload["operator_name"]
     assert data["operator_email"] == payload["operator_email"]
 
+
 def test_update_operator_missing_attribute(client, operator):
     """_summary_
         Try's to update a operator with missing attributes
@@ -210,32 +214,33 @@ def test_update_operator_missing_attribute(client, operator):
         client (_type_): _description_
         company {Company}: A Company Object from DB
     """
-    
+
     payload = {
-        "operator_name": "Keving Gates", 
+        "operator_name": "Keving Gates",
         "operator_email": "gator@gatertown.us"
     }
-    
+
     res = client.put(f"/{END_POINT}/{operator.operator_id}", json=payload)
     assert res.status_code == 400
-    
+
     payload = {
         "company_id": operator.company_id,
         "operator_email": "gator@gatertown.us"
     }
-    
+
     res = client.put(f"/{END_POINT}/{operator.operator_id}", json=payload)
     assert res.status_code == 400
-    
+
     payload = {
         "company_id": operator.company_id,
-        "operator_name": "Keving Gates", 
+        "operator_name": "Keving Gates",
     }
-    
+
     res = client.put(f"/{END_POINT}/{operator.operator_id}", json=payload)
     assert res.status_code == 400
-    
-def test_update_operator_invalid_attributes(client, operator): 
+
+
+def test_update_operator_invalid_attributes(client, operator):
     """_summary_
         Trying to update operators with invalid attributes
     Args:
@@ -244,79 +249,78 @@ def test_update_operator_invalid_attributes(client, operator):
     """
     payload = {
         "company_id": "ABC123",
-        "operator_name": "Keving Gates", 
+        "operator_name": "Keving Gates",
         "operator_email": "gator@gatertown.us"
     }
-    
+
     res = client.put(f"/{END_POINT}/{operator.operator_id}", json=payload)
     assert res.status_code == 400
-    
-    
+
     payload = {
         "company_id": operator.company_id,
-        "operator_name": "", 
+        "operator_name": "",
         "operator_email": "gator@gatertown.us"
     }
-    
+
     res = client.put(f"/{END_POINT}/{operator.operator_id}", json=payload)
     assert res.status_code == 400
-    
-    
+
     payload = {
         "company_id": operator.company_id,
-        "operator_name": "Keving Gates", 
+        "operator_name": "Keving Gates",
         "operator_email": "gatorgatertown.us"
     }
-    
+
     res = client.put(f"/{END_POINT}/{operator.operator_id}", json=payload)
     assert res.status_code == 400
-    
+
     payload = {
         "company_id": operator.company_id,
-        "operator_name": "Keving Gates", 
+        "operator_name": "Keving Gates",
         "operator_email": "gator@gatertownus"
     }
-    
+
     res = client.put(f"/{END_POINT}/{operator.operator_id}", json=payload)
     assert res.status_code == 400
-    
+
     payload = {
         "company_id": operator.company_id,
-        "operator_name": "Keving Gates", 
+        "operator_name": "Keving Gates",
         "operator_email": "gator@gatertown."
     }
-    
+
     res = client.put(f"/{END_POINT}/{operator.operator_id}", json=payload)
     assert res.status_code == 400
-    
-    
+
     payload = {
         "company_id": operator.company_id,
-        "operator_name": "Keving Gates", 
+        "operator_name": "Keving Gates",
         "operator_email": "@gatertown.us"
     }
-    
+
     res = client.put(f"/{END_POINT}/{operator.operator_id}", json=payload)
     assert res.status_code == 400
-    
-def test_update_non_existant_operator(client, company): 
+
+
+def test_update_non_existant_operator(client, company):
     """_summary_
         Testing to see if you can update a non existant operators
     Args:
         client (_type_): _description_
     """
-    
+
     payload = {
-        "company_id":company.company_id, 
-        "operator_name": "Kevin Stain", 
+        "company_id": company.company_id,
+        "operator_name": "Kevin Stain",
         "operator_email": "alegator@gatorbay.org"
     }
-    
+
     res = client.put(f"/{END_POINT}/999999", json=payload)
     data = res.json
-    
+
     assert res.status_code == 404
     assert "error" in data.keys()
+
 
 def test_update_a_operator_with_duplicate_email(client, operator):
     """_summary_
@@ -327,72 +331,74 @@ def test_update_a_operator_with_duplicate_email(client, operator):
     """
     # Create operator
     payload = {
-        "company_id": operator.company_id, 
-        "operator_name": "Keving Gates", 
+        "company_id": operator.company_id,
+        "operator_name": "Keving Gates",
         "operator_email": "test@test.us"
     }
-    
+
     print(operator)
     res = client.post(f"/{END_POINT}/", json=payload)
     data = res.json
-    
-    print(f"OPERATOR_ID: {operator_id}")
 
     assert res.status_code == 201
-    
+
     payload = {
-        "company_id":operator.company_id, 
-        "operator_name": "Keving Gates", 
+        "company_id": operator.company_id,
+        "operator_name": "Keving Gates",
         "operator_email": operator.operator_email
-    }   
-    
+    }
+
     operator_id = data["operator_id"]
     res = client.put(f"/{END_POINT}/{operator_id}", json=payload)
     data = res.json
 
     assert res.status_code == 400
     assert "error" in data.keys()
-    
-def test_delete_an_operator(client, operator): 
+
+
+def test_delete_an_operator(client, operator):
     """_summary_
         Try to delete an valid operator
     Args:
         client (): _description_
         operator (Operator): Operator Object
     """
-    
-    res = client.delete(f"/{END_POINT}/{operator.company_id}/{operator.operator_id}")
+
+    res = client.delete(
+        f"/{END_POINT}/{operator.company_id}/{operator.operator_id}")
     data = res.json
-    
+
     assert res.status_code == 200
     assert "message" in data.keys()
 
-def test_delete_an_non_existant_operator(client, company): 
+
+def test_delete_an_non_existant_operator(client, company):
     """_summary_
         Try to delete non existant operator
     Args:
         client (app): _description_
         operator (Operator): Operator Object
     """
-    
+
     res = client.delete(f"/{END_POINT}/{company.company_id}/999999")
     data = res.json
-    
+
     assert res.status_code == 404
     assert "error" in data.keys()
 
-def test_delete_an_operator_with_non_existant_company(client, operator): 
+
+def test_delete_an_operator_with_non_existant_company(client, operator):
     """_summary_
         Try to delete an operator from a company that does not own it.
     Args:
         client (app): _description_
         operator (Operator): Operator Object
     """
-    
+
     res = client.delete(f"/{END_POINT}/9999999/{operator.operator_id}")
     data = res.json
-    
+
     print(data)
-    
+
     assert res.status_code == 404
     assert "error" in data.keys()
