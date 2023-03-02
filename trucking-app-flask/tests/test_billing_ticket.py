a15f61b51634ac3165529f03e7a5337248b558ef
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-03-01 18:09:27 -0800

    Added test test_billing_ticket_get_non_existant

--- trucking-app-flask/tests/test_billing_ticket.py
+++ trucking-app-flask/tests/test_billing_ticket.py
@@ -4,8 +4,7 @@ from config_test import app, client, billing_ticket
 END_POINT = "v1/billing_ticket"
 
 
-@pytest.mark.usefixtures("client")
-def test_user_get(client, billing_ticket):
+def test_billing_ticket_get(client, billing_ticket):
     """_summary_
         Test geting a billing ticket
     Args:
@@ -22,3 +21,16 @@ def test_user_get(client, billing_ticket):
     assert data["rfo_id"] == billing_ticket.rfo_id
     assert "ticket_number" in data.keys()
     assert data["ticket_number"] == billing_ticket.ticket_number
+
+
+def test_billing_ticket_get_non_existant(client):
+    """_summary_
+        Test geting a billing ticket that does not exist
+    Args:
+        client (_type_): _description_
+    """
+    res = client.get(f"/{END_POINT}/{123456789}")
+    data = res.json
+
+    assert res.status_code == 404
+    assert "error" in data.keys()
