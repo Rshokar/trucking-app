02d6db5b783340cb5530c509ada69dc5bc2a0b77
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-03-25 15:32:42 -0700

    Added post test

--- trucking-app-flask/tests/test_user.py
+++ trucking-app-flask/tests/test_user.py
@@ -74,204 +74,206 @@ def test_user_invalid_id_format(client_authed):
     assert 404 == response.status_code
 
 
-# @pytest.mark.usefixtures("client")
-# def test_user_post(client):
-#     """
-#         Create a valid user
-#     """
-#     payload = {
-#         "role": "dispatcher",
-#         "password": "Testing1",
-#         "email": "test@demo.com"
+@pytest.mark.usefixtures("client")
+def test_user_post(client):
+    """
+        Create a valid user
+    """
+    payload = {
+        "role": "dispatcher",
+        "password": "Testing1",
+        "email": "test@demo.com"
 
-#     }
-#     headers = {"Content-Type": "application/json"}
-#     response = client.post(
-#         f"{END_POINT}/",
-#         headers=headers,
-#         json=payload
-#     )
+    }
+    headers = {"Content-Type": "application/json"}
+    response = client.post(
+        f"{END_POINT}/",
+        headers=headers,
+        json=payload
+    )
 
-#     # convert response to dictionary
-#     data = response.data.decode("utf-8")
-#     print(f"DATA: {data}")
-#     data = json.loads(data)
-#     # assertions
-#     assert 200 == response.status_code
-#     assert payload['role'] == data["role"]
-#     assert payload['email'] == data["email"]
-
-
-# @pytest.mark.usefixtures("client")
-# def test_user_post_invalid_role(client):
-#     """
-#     See if errors are returned for invalid role user data
-#     """
-
-#     # Invaid Role
-#     headers = {"Content-Type": "application/json"}
-#     payload = {
-#         "role": "None",
-#         "password": "Testing1",
-#         "email": "test@demo.com",
-#     }
-#     response = client.post(f"{END_POINT}/", headers=headers, json=payload)
-#     data = json.loads(response.data.decode("utf-8"))
-#     assert 400 == response.status_code
-#     assert "error" in data.keys()
-
-#     # Invaid Email
-#     headers = {"Content-Type": "application/json"}
-#     payload = {
-#         "role": "dispatcher",
-#         "password": "Testing1",
-#         "email": "testdemo.com",
-#     }
-#     response = client.post(f"{END_POINT}/", headers=headers, json=payload)
-#     data = json.loads(response.data.decode("utf-8"))
-#     assert 400 == response.status_code
-#     assert "error" in data.keys()
-
-#     payload = {
-#         "role": "dispatcher",
-#         "password": "Testing1",
-#         "email": "test@democom",
-#     }
-#     response = client.post(f"{END_POINT}/", headers=headers, json=payload)
-#     data = json.loads(response.data.decode("utf-8"))
-#     assert 400 == response.status_code
-#     assert "error" in data.keys()
-
-#     payload = {
-#         "role": "dispatcher",
-#         "password": "Testing1",
-#         "email": "@demo.com",
-#     }
-#     response = client.post(f"{END_POINT}/", headers=headers, json=payload)
-#     data = json.loads(response.data.decode("utf-8"))
-#     assert 400 == response.status_code
-#     assert "error" in data.keys()
-
-#     payload = {
-#         "role": "dispatcher",
-#         "password": "Testing1",
-#         "email": "test@demo.",
-#     }
-#     response = client.post(f"{END_POINT}/", headers=headers, json=payload)
-#     data = json.loads(response.data.decode("utf-8"))
-#     assert 400 == response.status_code
-#     assert "error" in data.keys()
+    # convert response to dictionary
+    data = response.data.decode("utf-8")
+    print(f"DATA: {data}")
+    data = json.loads(data)
+    # assertions
+    assert 200 == response.status_code
+    assert payload['role'] == data["role"]
+    assert payload['email'] == data["email"]
 
 
-# @pytest.mark.usefixtures("client")
-# def test_user_post_invalid_password(client):
-#     """_summary_
+@pytest.mark.usefixtures("client")
+def test_user_post_invalid_role(client):
+    """
+    See if errors are returned for invalid role user data
+    """
 
-#     See if error is returned when a in invalid password is entered
-#     """
-#     # No Number
-#     headers = {"Content-Type": "application/json"}
-#     payload = {
-#         "role": "None",
-#         "password": "Testing",
-#         "email": "test@demo.com",
-#     }
-#     response = client.post(f"{END_POINT}/", headers=headers, json=payload)
-#     data = json.loads(response.data.decode("utf-8"))
-#     assert 400 == response.status_code
-#     assert "error" in data.keys()
+    # Invaid Role
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "role": "None",
+        "password": "Testing1",
+        "email": "test@demo.com",
+    }
+    response = client.post(f"{END_POINT}/", headers=headers, json=payload)
+    data = json.loads(response.data.decode("utf-8"))
+    assert 400 == response.status_code
+    assert "error" in data.keys()
 
-#     # No uppercase
-#     headers = {"Content-Type": "application/json"}
-#     payload = {
-#         "role": "None",
-#         "password": "testing1",
-#         "email": "test@demo.com",
-#     }
-#     response = client.post(f"{END_POINT}/", headers=headers, json=payload)
-#     data = json.loads(response.data.decode("utf-8"))
-#     assert 400 == response.status_code
-#     assert "error" in data.keys()
+    # Invaid Email
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "role": "dispatcher",
+        "password": "Testing1",
+        "email": "testdemo.com",
+    }
+    response = client.post(f"{END_POINT}/", headers=headers, json=payload)
+    data = json.loads(response.data.decode("utf-8"))
+    assert 400 == response.status_code
+    assert "error" in data.keys()
 
-#     # No lowercase
-#     headers = {"Content-Type": "application/json"}
-#     payload = {
-#         "role": "None",
-#         "password": "TESTING1",
-#         "email": "test@demo.com",
-#     }
-#     response = client.post(f"{END_POINT}/", headers=headers, json=payload)
-#     data = json.loads(response.data.decode("utf-8"))
-#     assert 400 == response.status_code
-#     assert "error" in data.keys()
+    payload = {
+        "role": "dispatcher",
+        "password": "Testing1",
+        "email": "test@democom",
+    }
+    response = client.post(f"{END_POINT}/", headers=headers, json=payload)
+    data = json.loads(response.data.decode("utf-8"))
+    assert 400 == response.status_code
+    assert "error" in data.keys()
 
-#     # Less than 8 charactesr
-#     headers = {"Content-Type": "application/json"}
-#     payload = {
-#         "role": "None",
-#         "password": "Test1",
-#         "email": "test@demo.com",
-#     }
-#     response = client.post(f"{END_POINT}/", headers=headers, json=payload)
-#     data = json.loads(response.data.decode("utf-8"))
-#     assert 400 == response.status_code
-#     assert "error" in data.keys()
+    payload = {
+        "role": "dispatcher",
+        "password": "Testing1",
+        "email": "@demo.com",
+    }
+    response = client.post(f"{END_POINT}/", headers=headers, json=payload)
+    data = json.loads(response.data.decode("utf-8"))
+    assert 400 == response.status_code
+    assert "error" in data.keys()
 
-
-# @pytest.mark.usefixtures("client")
-# def test_user_post_duplicate(client, user):
-#     """
-#         Checks to see if an error is returned when creating a
-#         duplicate user
-#     """
-#     payload = user.to_dict()
-#     payload["password"] = "Testing1"
-#     # Create user
-#     headers = {"Content-Type": "application/json"}
-#     response = client.post(
-#         f"{END_POINT}/",
-#         headers=headers,
-#         json=payload
-#     )
-
-#     # convert response to dictionary
-#     data = response.data.decode("utf-8")
-#     data = json.loads(data)
-
-#     # assertions
-#     assert response.status == '409 CONFLICT'
-#     assert "error" in data.keys()
+    payload = {
+        "role": "dispatcher",
+        "password": "Testing1",
+        "email": "test@demo.",
+    }
+    response = client.post(f"{END_POINT}/", headers=headers, json=payload)
+    data = json.loads(response.data.decode("utf-8"))
+    assert 400 == response.status_code
+    assert "error" in data.keys()
 
 
-# @pytest.mark.usefixtures("client")
-# def test_user_put(client, user):
-#     """_summary_
-#         Test a valid post request
-#     """
-#     headers = {"Content-Type": "application/json"}
-#     payload = user.to_dict()
-#     payload["email"] = "update@update.com"
-#     payload["role"] = "dispatcher"
-#     print(user.to_dict())
-#     print(payload)
-#     response = client.put(
-#         f"{END_POINT}/{user.id}",
-#         headers=headers,
-#         json=payload
-#     )
+@pytest.mark.usefixtures("client")
+def test_user_post_invalid_password(client):
+    """_summary_
 
-#     # convert response to dictionary
-#     data = response.data.decode("utf-8")
-#     data = json.loads(data)
-#     print(f"DATA: {data}")
+    See if error is returned when a in invalid password is entered
+    """
+    # No Number
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "role": "None",
+        "password": "Testing",
+        "email": "test@demo.com",
+    }
+    response = client.post(f"{END_POINT}/", headers=headers, json=payload)
+    data = json.loads(response.data.decode("utf-8"))
+    assert 400 == response.status_code
+    assert "error" in data.keys()
 
-#     # assertions
-#     assert 200 == response.status_code
-#     assert "email" in data.keys()
-#     assert payload["email"] == data["email"]
-#     assert "role" in data.keys()
-#     assert payload["role"] == data["role"]
-#     assert "id" in data.keys()
+    # No uppercase
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "role": "None",
+        "password": "testing1",
+        "email": "test@demo.com",
+    }
+    response = client.post(f"{END_POINT}/", headers=headers, json=payload)
+    data = json.loads(response.data.decode("utf-8"))
+    assert 400 == response.status_code
+    assert "error" in data.keys()
+
+    # No lowercase
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "role": "None",
+        "password": "TESTING1",
+        "email": "test@demo.com",
+    }
+    response = client.post(f"{END_POINT}/", headers=headers, json=payload)
+    data = json.loads(response.data.decode("utf-8"))
+    assert 400 == response.status_code
+    assert "error" in data.keys()
+
+    # Less than 8 charactesr
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "role": "None",
+        "password": "Test1",
+        "email": "test@demo.com",
+    }
+    response = client.post(f"{END_POINT}/", headers=headers, json=payload)
+    data = json.loads(response.data.decode("utf-8"))
+    assert 400 == response.status_code
+    assert "error" in data.keys()
+
+
+@pytest.mark.usefixtures("client")
+def test_user_post_duplicate(client, user):
+    """
+        Checks to see if an error is returned when creating a
+        duplicate user
+    """
+    payload = user.to_dict()
+    payload["password"] = "Testing1"
+    # Create user
+    headers = {"Content-Type": "application/json"}
+    response = client.post(
+        f"{END_POINT}/",
+        headers=headers,
+        json=payload
+    )
+
+    # convert response to dictionary
+    data = response.data.decode("utf-8")
+    data = json.loads(data)
+
+    # assertions
+    assert response.status == '409 CONFLICT'
+    assert "error" in data.keys()
+
+
+@pytest.mark.usefixtures("client")
+def test_user_put(client_authed):
+    """_summary_
+        Test a valid post request
+    """
+    # Arrange
+    client, user = client_authed
+    headers = {"Content-Type": "application/json"}
+    payload = user.to_dict()
+    payload["email"] = "update@update.com"
+    payload["role"] = "dispatcher"
+    print(user.to_dict())
+    print(payload)
+
+    # Act
+    response = client.put(
+        f"{END_POINT}/{user.id}",
+        headers=headers,
+        json=payload
+    )
+
+    # assertions
+    assert 200 == response.status_code
+    data = response.data.decode("utf-8")
+    data = json.loads(data)
+    print(f"DATA: {data}")
+    assert "email" in data.keys()
+    assert payload["email"] == data["email"]
+    assert "role" in data.keys()
+    assert payload["role"] == data["role"]
+    assert "id" in data.keys()
 
 
 # @pytest.mark.usefixtures("client")
