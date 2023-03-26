cee830b7c4b2be7dd774c96dbf5ea93bd13ea52f
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-03-26 00:18:14 -0700

    Added auth test

--- trucking-app-flask/tests/test_auth.py
+++ trucking-app-flask/tests/test_auth.py
@@ -1,26 +1,96 @@
 import pytest
+import requests
 import json
-from config_test import app, client
+from config_test import app, client, user, client_authed
 END_POINT = "v1/auth"
 
 
-@pytest.mark.usefixtures("client")
-def test_auth_login(client):
-    response = client.post("/{}/login".format(END_POINT))
+def test_auth_login(client, user):
+    """_summary_
+    Test user login with valid credentials
+    Args:
+        client (app): Flask app
+        user (User): User model
+    """
 
-    # convert response to dicitionary
-    data = response.data.decode("utf-8")
-    data = json.loads(data)
+    # Arrange
+    payload = {
+        "email": user.email,
+        "password": "Testing1"
+    }
 
-    # assertions
-    assert "LOGED_IN" == data["data"]
-    assert 200 == response.status_code
+    # Act
+    res = client.post(f"/{END_POINT}/login", json=payload)
+
+    # Assert
+
+    cookie = next((cookie for cookie in client.cookie_jar if cookie.name == "session"),
+                  None)
+    assert cookie is not None
+    assert 200 == res.status_code
 
 
-@pytest.mark.usefixtures("client")
-def test_auth_logout(client):
-    response = client.delete("/{}/logout".format(END_POINT))
+def test_auth_login_missing_attributes(client, user):
+    """_summary_
+    Test user login with missing attributes
+    Args:
+        client (_type_): _description_
+        user (_type_): _description_
+    """
 
-    # No content. We are deleting users token fron db
-    # therefore no content
-    assert 204 == response.status_code
+    # Arrange (missing password)
+    payload = {
+        "email": user.email,
+    }
+
+    # Act
+    res = client.post(f"/{END_POINT}/login", json=payload)
+
+    # Assert
+    assert 400 == res.status_code
+
+    # Arrange (missing email)
+    payload = {
+        "password": "Testing1"
+    }
+
+    # Act
+    res = client.post(f"/{END_POINT}/login", json=payload)
+
+    # Assert
+    assert 400 == res.status_code
+
+
+def test_auth_logout(client_authed):
+    """_summary_
+    Test user logout
+    Args:
+        client_authed (_type_): _description_
+    """
+    # Arrange
+    client, user = client_authed
+
+    # Act
+    res = client.delete(f"/{END_POINT}/logout")
+
+    # Assert
+    assert 200 == res.status_code
+    # check if cookie is deleted
+    cookie = next((cookie for cookie in client.cookie_jar if cookie.name == "session"),
+                  None)
+    assert cookie is None
+
+
+def test_auth_logout_unauthorized(client):
+    """_summary_
+    Test user logout without being logged in
+    Args:
+        client (_type_): _description_
+    """
+    # Arrange
+
+    # Act
+    res = client.delete(f"/{END_POINT}/logout")
+
+    # Assert
+    assert 401 == res.status_code
