e5ca0b75cbfb0bf2127bab2337c62df6abc74e17
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-03-25 18:32:03 -0700

    Added test to validate authentication on company get and put routes

--- trucking-app-flask/tests/test_company.py
+++ trucking-app-flask/tests/test_company.py
@@ -63,7 +63,7 @@ def test_company_get_another_users_company(client_authed, company):
     response = client.get(f"/{END_POINT}/{company.company_id}")
 
     # Assert
-    assert response.status_code == 401
+    assert response.status_code == 403
 
 
 def test_company_get_non_company_found(client_authed):
@@ -96,76 +96,287 @@ def test_company_get_unauthorized_user(client, company):
     # Assert
     assert res.status_code == 401
 
-# def test_company_get_invalid_id(client):
-#     """_summary_
-#         Attempts to make a get request with an invalid parameter
-#     Args:
-#         client (_type_): _description_
-#     """
 
-#     res = client.get(f"/{END_POINT}/invalid")
-#     data = res.json
+def test_company_get_invalid_id(client_authed):
+    """_summary_
+        Attempts to make a get request with an invalid parameter
+    Args:
+        client_authed (Array): an array containing the authenticated client and user
+    """
+    # Arrange
+    client, user = client_authed
+    company = CompanyFactory.create(owner_id=user.id)
 
-#     assert res.status_code == 404
+    # Act
+    res = client.get(f"/{END_POINT}/invalid")
 
-
-# def test_company_post(client, user):
-#     """_summary_
-#         Attempts to create a new company
-#     Args:
-#         client (_type_): _description_
-#     """
-
-#     payload = {
-#         "owner_id": user.id,
-#         "company_name": "AKS Trucking Ltd"
-#     }
-
-#     res = client.post(f"/{END_POINT}/", json=payload)
-#     data = res.json
-
-#     assert res.status_code == 200
-#     assert "company_id" in data.keys()
-#     assert "owner_id" in data.keys()
-#     assert data["owner_id"] == payload["owner_id"]
-#     assert "company_name" in data.keys()
-#     assert data["company_name"] == payload["company_name"]
+    # Assert
+    assert res.status_code == 404
 
 
-# def test_company_put(client, company):
-#     headers = {"Content-Type": "application/json"}
-#     payload = {"company_name": "AKS Trucking Ltd"}
-#     response = client.put(
-#         f"/{END_POINT}/{company.company_id}",
-#         headers=headers,
-#         json=payload
-#     )
+def test_company_post(client_authed):
+    """_summary_
+        Attempts to create a company
+    Args:
+        client_authed (Array): an array containing the authenticated client and user
+    """
+    client, user = client_authed
+    payload = {
+        "owner_id": user.id,
+        "company_name": "AKS Trucking Ltd"
+    }
 
-#     data = json.loads(response.data)
+    res = client.post(f"/{END_POINT}/", json=payload)
+    data = res.json
 
-#     assert response.status_code == 200
-#     assert "company_id" in data.keys()
-#     assert type(data["company_id"]) == int
-#     assert "owner_id" in data.keys()
-#     assert type(data["owner_id"]) == int
-#     assert "company_name" in data.keys()
-#     assert payload["company_name"] == data["company_name"]
+    assert res.status_code == 200
+    assert "company_id" in data.keys()
+    assert "owner_id" in data.keys()
+    assert data["owner_id"] == payload["owner_id"]
+    assert "company_name" in data.keys()
+    assert data["company_name"] == payload["company_name"]
 
 
-# def test_company_put_nonexistent(client):
-#     headers = {"Content-Type": "application/json"}
-#     payload = {"company_name": "AKS Trucking Ltd"}
-#     response = client.put(
-#         f"/{END_POINT}/1000",
-#         headers=headers,
-#         json=payload
-#     )
-#     data = json.loads(response.data)
+def test_company_post_invalid_attributes(client_authed):
+    """_summary_
+        Attempts to create a company with invalid arguments
+    Args:
+        client_authed (Array): an array containing the authenticated client and user
+    """
 
-#     assert response.status_code == 404
-#     assert "error" in data.keys()
-#     assert data["error"] == "Company not found."
+    # Arrange (company_name is empty)
+    client, user = client_authed
+    payload = {
+        "owner_id": user.id,
+        "company_name": ""
+    }
 
+    # Act
+    res = client.post(f"/{END_POINT}/", json=payload)
+
+    # Assert
+    assert res.status_code == 400
+
+    # Arrange (owner_id is empty)
+    payload = {
+        "company_name": "AKS Trucking Ltd",
+        "owner_id": ""
+    }
+
+    # Act
+    res = client.post(f"/{END_POINT}/", json=payload)
+
+    # Assert
+    assert res.status_code == 400
+
+    # Arrange (owner_id is invalid)
+    payload = {
+        "company_name": "AKS Trucking Ltd",
+        "owner_id": "invalid"
+    }
+
+    # Act
+    res = client.post(f"/{END_POINT}/", json=payload)
+
+    # Assert
+    assert res.status_code == 400
+
+
+def test_company_post_missing_attributes(client_authed):
+    """_summary_
+        Attempts to create a company with missing arguments
+    Args:
+        client_authed (_type_): _description_
+    """
+    # Arrange (missing company_name)
+    client, user = client_authed
+    payload = {
+        "owner_id": user.id
+    }
+
+    # Act
+    res = client.post(f"/{END_POINT}/", json=payload)
+
+    # Assert
+    assert res.status_code == 400
+
+    # Arrange (missing owner_id)
+
+    # Act
+    res = client.post(f"/{END_POINT}/", json=payload)
+
+    # Assert
+    assert res.status_code == 400
+
+
+def test_company_post_already_have_company(client_authed):
+    """_summary_
+        Attempts to create a company when the user already has a company
+    Args:
+        client_authed (_type_): _description_
+    """
+    # Arrange
+    client, user = client_authed
+    comp = CompanyFactory.create(owner_id=user.id)
+    payload = {
+        "owner_id": user.id,
+        "company_name": "AKS Trucking Ltd"
+    }
+
+    # Act
+    res = client.post(f"/{END_POINT}/", json=payload)
+
+    # Assert
+    assert res.status_code == 409
+
+
+def test_company_post_unauthorized_user(client):
+    """_summary_
+        Attempts to create a company when un authorized
+    Args:
+        client (Flask App): the flask app
+    """
+    # Arrange
+    payload = {
+        "owner_id": 1,
+        "company_name": "AKS Trucking Ltd"
+    }
+
+    # Act
+    res = client.post(f"/{END_POINT}/", json=payload)
+
+    # Assert
+    assert res.status_code == 401
+
+
+def test_company_put(client_authed):
+    """_summary_
+        Attempts to update a company
+    Args:
+        client_authed (Array): an array containing the authenticated client and user
+    """
+
+    # Arrange
+    client, user = client_authed
+    comp = CompanyFactory.create(owner_id=user.id)
+    payload = {
+        "company_name": "AKS Trucking Ltd"
+    }
+
+    # Act
+    res = client.put(f"/{END_POINT}/{comp.company_id}", json=payload)
+
+    # Assert
+    assert res.status_code == 200
+
+
+def test_company_put_invalid_attributes(client_authed):
+    """_summary_
+        Attempts to update a company with invalid attributes
+
+    Args:
+        client_authed (Array): an array containing the authenticated client and user
+    """
+
+    # Arrange (Empty company_name)
+    client, user = client_authed
+    comp = CompanyFactory.create(owner_id=user.id)
+    payload = {
+        "company_name": ""
+    }
+
+    # Act
+    res = client.put(f"/{END_POINT}/{comp.company_id}", json=payload)
+
+    # Assert
+    assert res.status_code == 400
+
+    # Arrange (Company name too long)
+    payload = {
+        "company_name": "a" * 51
+    }
+
+    # Act
+    res = client.put(f"/{END_POINT}/{comp.company_id}", json=payload)
+
+    # Assert
+    assert res.status_code == 400
+
+
+def test_company_put_missing_attributes(client_authed):
+    """_summary_
+        Attempts to update a company with missing attributes
+    Args:
+        client_authed (Array): an array containing the authenticated client and user
+    """
+    # Arrange
+    client, user = client_authed
+    comp = CompanyFactory.create(owner_id=user.id)
+    payload = {}
+
+    # Act
+    res = client.put(f"/{END_POINT}/{comp.company_id}", json=payload)
+
+    # Assert
+    assert res.status_code == 400
+
+
+def test_company_put_nonexistent(client_authed):
+    """_summary_
+        Attempts to update a company that does not exist
+
+    Args:
+        client_authed (Array): an array containing the authenticated client and user
+    """
+
+    # Arrange
+    client, user = client_authed
+    payload = {
+        "company_name": "AKS Trucking Ltd"
+    }
+
+    # Act
+    res = client.put(f"/{END_POINT}/1000", json=payload)
+
+    # Assert
+    assert res.status_code == 404
+
+
+def test_company_put_unauthorized_user(client, company):
+    """_summary_
+        Attempts to update a company when un authorized
+    Args:
+        client (Flask App): the flask app
+    """
+    # Arrange
+    payload = {
+        "company_name": "AKS Trucking Ltd"
+    }
+
+    # Act
+    res = client.put(f"/{END_POINT}/{company.company_id}", json=payload)
+
+    # Assert
+    assert res.status_code == 401
+
+
+def test_company_put_another_user_company(client_authed, company):
+    """_summary_
+        Attempts to update a company that does not belong to the user
+    Args:
+        client_authed (Array): an array containing the authenticated client and user
+    """
+
+    # Arrange
+    client, user = client_authed
+    CompanyFactory.create(owner_id=user.id)
+    payload = {"company_name": "AKS Trucking Ltd"}
+
+    # Act
+    res = client.put(f"/{END_POINT}/{company.company_id}", json=payload)
+
+    # Assert
+    assert res.status_code == 403
 
 # def test_company_delete(client, company):
 #     response = client.delete(f"/{END_POINT}/{company.company_id}")
