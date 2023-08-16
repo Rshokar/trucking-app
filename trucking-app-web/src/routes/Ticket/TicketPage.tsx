02b063164a569a1d867a5220d9e3dc0e3b77f399
Ravinder Shokar <ravindershokar@gmail.com>
2023-08-16 07:25:33 -0700

    Rendered ticket on web app ticket page

--- trucking-app-web/src/routes/Ticket/TicketPage.tsx
+++ trucking-app-web/src/routes/Ticket/TicketPage.tsx
@@ -8,27 +8,59 @@ import { useParams, useNavigation } from 'react-router-dom';
 import { Bill } from '../../models/Bill';
 import { Dispatch } from '../../models/Dispatch';
 import { Customer } from '../../models/Customer';
-import { Company } from '../../models/Company';
 import { RFO } from '../../models/RFO';
-
+import { Company } from '../../models/Company';
+import MessageView from '../../components/MessageView/MessageView';
+import IconButton from '@mui/material/IconButton';
+import moment from 'moment';
+import BookIcon from '@mui/icons-material/Book';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import { colors } from '../..';
 const View = styled(Container)`
     height: 100vh;
     width: 100vw;
 `
 
-const MessageView = styled(Box)`
-    display: flex; 
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center; 
+const TicketInfoContainer = styled.div`
     max-width: 90%; 
-    height: 350px; 
-    width: 400px;  
-    border-radius: 10px;
+    width: 500px; 
+    display: flex;
+    justify-content: center;
+    align-items: center; 
+    flex-direction: column;
+    padding: 20px; 
     box-sizing: border-box;
-    padding: 30px;   
-    box-shadow: 2px 2px 2px 2px grey;
+    gap: 10px;
 `
+
+const TicketContainer = styled(Box)`
+    width: 100%; 
+    height: auto;
+    padding: 10px; 
+    border: 2px solid grey;
+    border-radius: 5px;
+`
+
+const TicketContainerHeader = styled.div`
+    display: flex; 
+    flex-direction: row; 
+    gap: 10px;
+`
+
+
+const TextSection = styled.div`
+    padding-top: 10px; 
+    display: flex;
+    flex-direction: column;
+`
+
+const SeperatedText = styled.div`
+    display: flex; 
+    flex-direction: row; 
+    justify-content: space-between; 
+    align-items: center; 
+`
+
 
 type Props = {}
 
@@ -36,12 +68,14 @@ const TicketPage = (props: Props) => {
     const theme = useTheme();
     const [validated, setValidated] = useState(false); // flag that indicates whether an email has been sent to operator
     const [accessToken, setAccessToken] = useState<string>();
+    const [loaded, setLoaded] = useState<boolean>(false);
 
     const [disp, setDispatch] = useState<Dispatch>();
     const [rfo, setRFO] = useState<RFO>();
     const [customer, setCustomer] = useState<Customer>();
     const [bills, setBills] = useState<Bill>();
     const [operator, setOperator] = useState<string>();
+    const [company, setCompany] = useState<Company>();
 
     const { token } = useParams();
 
@@ -78,7 +112,8 @@ const TicketPage = (props: Props) => {
             setCustomer(result.customer)
             setBills(result.bills)
             setOperator(result.operator)
-
+            setCompany(result.company)
+            setLoaded(true)
         } catch (err: any) {
             console.log(err);
         }
@@ -101,34 +136,62 @@ const TicketPage = (props: Props) => {
     console.log('VALIDATE', validated, accessToken);
 
     return <View>
-        <MessageView style={{ backgroundColor: theme.palette.secondary.main }}>
-            <div style={{
-                display: 'flex',
-                gap: '10px',
-                flexDirection: 'column',
-            }}>
-                <Typography variant='h5' style={{ color: 'white' }}>
-                    {validated ? "Thanks for your patients!" : "Validating....."}
-                </Typography>
-                <Typography variant='body2' style={{ color: 'white' }}>
-                    {validated ? "Thank you for your patients! Here is your ticket" : "Just give us a second, we are validating your token."}
-                </Typography>
-            </div>
-            <Box sx={{
-                display: 'flex',
-                backgroundColor: 'white',
-                width: '60px',
-                height: '60px',
-                alignItems: 'center',
-                justifyContent: 'center',
-                borderRadius: '10px'
-            }}>
-                {validated ? <LockOpenIcon style={{ fontSize: '25pt' }} /> : <LockIcon style={{ fontSize: '25pt' }} />}
-            </Box>
-            <Typography variant='caption' style={{ color: 'white' }}>
-                {validated ? "All done thanks for waiting" : "Sorry for taking too long...."}
-            </Typography>
-        </MessageView>
+        {loaded ?
+            <TicketInfoContainer>
+                <TicketContainer>
+                    <TicketContainerHeader>
+                        <IconButton>
+                            <BookIcon style={{ fontSize: '25pt', color: theme.palette.secondary.main }} />
+                        </IconButton>
+                        <div>
+                            <Typography variant='h5' style={{ fontWeight: 'bold' }}>{customer?.customer_name}</Typography>
+                            <Typography variant='subtitle2'>{moment(disp?.date).format("YYYY-MM-DD")}</Typography>
+                        </div>
+                    </TicketContainerHeader>
+                    <TextSection>
+                        <Typography variant='body2'>Dispatcher: {company?.company_name}</Typography>
+                        <Typography variant='body2'>Notes: {disp?.notes}</Typography>
+                    </TextSection>
+                </TicketContainer>
+                <TicketContainer>
+                    <TicketContainerHeader>
+                        <IconButton>
+                            <LocalShippingIcon style={{ fontSize: '25pt', color: theme.palette.secondary.main }} />
+                        </IconButton>
+                        <div>
+                            <Typography variant='h5' style={{ fontWeight: 'bold' }}>{operator}</Typography>
+                            <Typography variant='subtitle2'>Equipment: {rfo?.truck} {rfo?.trailer}</Typography>
+                        </div>
+                    </TicketContainerHeader>
+                    <TextSection>
+                        <SeperatedText>
+                            <Typography variant='body2'>Start Location:</Typography>
+                            <Typography variant='body2'>{rfo?.start_location}</Typography>
+                        </SeperatedText>
+                        <SeperatedText>
+                            <Typography variant='body2'>Load Location:</Typography>
+                            <Typography variant='body2'>{rfo?.load_location}</Typography>
+                        </SeperatedText>
+                        <SeperatedText>
+                            <Typography variant='body2'>Dump Location:</Typography>
+                            <Typography variant='body2'>{rfo?.dump_location}</Typography>
+                        </SeperatedText>
+                        <SeperatedText>
+                            <Typography variant='body2'>Start Time:</Typography>
+                            <Typography variant='body2'> {moment(rfo?.start_time).format("YYYY-MM-DD h:mm a")}</Typography>
+                        </SeperatedText>
+                    </TextSection>
+                </TicketContainer>
+            </TicketInfoContainer>
+            :
+            <MessageView
+                title={validated ? "Thanks for your patients!" : "Validating....."}
+                subtitle={validated ? "Thank you for your patients! Here is your ticket" : "Just give us a second, we are validating your token."}
+                icon={validated ? <LockOpenIcon style={{ fontSize: '25pt' }} /> : <LockIcon style={{ fontSize: '25pt' }} />}
+                message={validated ? "All done thanks for waiting" : "Sorry for taking too long...."}
+            />
+
+        }
     </View>
 }
 
