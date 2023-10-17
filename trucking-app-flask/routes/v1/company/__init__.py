6417f298624120fbb083ec906cd3181f949c6b57
Ethan Lee <33e.lee@gmail.com>
2023-10-16 18:12:41 -0700

    create separate route for contact

--- trucking-app-flask/routes/v1/company/__init__.py
+++ trucking-app-flask/routes/v1/company/__init__.py
@@ -1,5 +1,7 @@
 from .company import company
 from .customers import customers
 from .operators import operators
+from .contact import contact
 company.register_blueprint(customers, url_prefix="/customers")
 company.register_blueprint(operators, url_prefix="/operators")
+company.register_blueprint(contact, url_prefix="/contact")
