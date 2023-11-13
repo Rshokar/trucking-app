7f21b47e29be371317f552fd7cc9cc0dfb8205d0
Ravinder Shokar <ravindershokar@gmail.com>
2023-11-12 18:03:37 -0800

    Added feature to register endpoint. A stripe user will also be created

--- trucking-app-flask/config/db.py
+++ trucking-app-flask/config/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from models.model import Base
 import os
 
-IS_PRODUCTION = os.environ.get("STATE")
+IS_PRODUCTION = os.environ.get("STATE") == 'production'
 TEST_DATA_NUM_USERS = os.environ.get("TEST_DATA_NUM_USERS")
 
 
@@ -23,6 +23,14 @@ engine = create_engine(connection_string)
 # Connect to database
 connection = engine.connect()
 print("--|--Connection to the database is successful--|--")
+
+if not IS_PRODUCTION:
+    print("Dropping and rebuilding the database...")
+    Base.metadata.drop_all(engine)  # Drop all tables
+    Base.metadata.create_all(engine)  # Create all tables
+    print("Database has been reset.")
+else:
+    print("Skipped database reset in production environment.")
 
 
 # Create a session
--- trucking-app-flask/models/user.py
+++ trucking-app-flask/models/user.py
@@ -27,11 +27,20 @@ class User(Base):
         'LENGTH(email_validation_token) = 6'), nullable=True)
     email_validation_token_consumed = Column(
         'email_validation_token_cosumed', Boolean, default=True)
+    stripe_id = Column("stripe_id", String(18), nullable=False)
+    
+    @validates("stripe_id")
+    def validate_stripe_id(self, key, stripe_id):
+        LENGTH = 18
+        if len(stripe_id) != LENGTH:
+             raise ValueError("Invalid stripe id")
+         
+        return stripe_id
 
     @validates("role")
     def validate_role(self, key, role):
         if role != UserRole.DISPATCHER.value:
-            raise ValueError("Invalid Role")
+            raise ValueError("Invalid r ole")
         return role
 
     @validates("reset_code")
@@ -44,14 +53,15 @@ class User(Base):
     company = relationship("Company", backref="owner",
                            lazy=True, cascade="delete", uselist=False)
 
-    def __init__(self, id, role=UserRole.DISPATCHER.value, reset_code=None, recovery_token=None):
+    def __init__(self, id, stripe_id, role=UserRole.DISPATCHER.value, reset_code=None, recovery_token=None):
         self.role = role
         self.id = id
         self.reset_code = reset_code
         self.recovery_token = recovery_token
+        self.stripe_id = stripe_id
 
     def __repr__(self):
-        return f"USER: ({self.id}) {self.role}, {self.reset_code}, {self.recovery_token}"
+        return f"USER: ({self.id}) {self.role}, {self.reset_code}, {self.recovery_token}, {self.stripe_id}"
 
     def to_dict(self):
         return {
