8fc914125d2fe89038974d4fd1244c93cd1551b3
Rshokar <60485121+Rshokar@users.noreply.github.com>
2024-01-29 08:34:39 -0800

    Update firebaseConfig.ts

--- trucking-app-client/config/firebaseConfig.ts
+++ trucking-app-client/config/firebaseConfig.ts
@@ -7,12 +7,7 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBw_orl37OX48-9bmzkItQ-DH-l-66CK2E",
-    authDomain: "tare-ticketing.firebaseapp.com",
-    projectId: "tare-ticketing",
-    storageBucket: "tare-ticketing.appspot.com",
-    messagingSenderId: "86124734373",
-    appId: "1:86124734373:web:e3efcd2ea9816214852a4c"
+
 };
 
 // Initialize Firebase
